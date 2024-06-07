15d4e4c534fd28fae8ce7f7f0051474a3970d995
ashutosh887 <ashutoshj887@gmail.com>
2024-06-07 13:17:37 +0530

    add: URLs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 function App() {
-  const url = "https://global-stg.transak.com";
+  const transakURL = "https://global-stg.transak.com";
+  const apiKey = "d79671a4-b021-4a4f-a444-6862a680a94b";
+  const redirectURL = "https://transak-redirect.vercel.app/redirect";
+  const environment = "STAGING";
+
+  const fullURL = `${transakURL}/?apiKey=${apiKey}&environment=${environment}&redirectURL=${encodeURIComponent(
+    redirectURL
+  )}`;
 
   return (
     <div className="container">
       <a
-        href={url}
+        href={fullURL}
         target="_blank"
         rel="noopener noreferrer"
         className="transak"
