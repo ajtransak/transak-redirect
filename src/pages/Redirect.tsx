4a93e53d10158d615a926e8d813adee12f69a2a6
ashutosh887 <ashutoshj887@gmail.com>
2024-06-07 14:09:17 +0530

    fix: redirect page styles

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 
@@ -17,7 +18,28 @@ function Redirect() {
   const isNFTOrder = searchParams.get("isNFTOrder");
   const network = searchParams.get("network");
 
-  const areParamsPresent = [
+  const [missingParams, setMissingParams] = useState<string[]>([]);
+
+  useEffect(() => {
+    const params = {
+      orderId,
+      fiatCurrency,
+      cryptoCurrency,
+      fiatAmount,
+      cryptoAmount,
+      isBuyOrSell,
+      status,
+      walletAddress,
+      totalFeeInFiat,
+      isNFTOrder,
+      network,
+    };
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    const missing = Object.keys(params).filter((key) => params[key] === null);
+    setMissingParams(missing);
+  }, [
     orderId,
     fiatCurrency,
     cryptoCurrency,
@@ -29,68 +51,74 @@ function Redirect() {
     totalFeeInFiat,
     isNFTOrder,
     network,
-  ].every((param) => param !== null);
+  ]);
+
+  const areParamsPresent = missingParams.length === 0;
 
   return (
     <div className="container">
-      {areParamsPresent ? (
-        <>
-          <table>
-            <tbody>
-              <tr>
-                <td>Order ID</td>
-                <td>{orderId}</td>
-              </tr>
-              <tr>
-                <td>Fiat Currency</td>
-                <td>{fiatCurrency}</td>
-              </tr>
-              <tr>
-                <td>Crypto Currency</td>
-                <td>{cryptoCurrency}</td>
-              </tr>
-              <tr>
-                <td>Fiat Amount</td>
-                <td>{fiatAmount}</td>
-              </tr>
-              <tr>
-                <td>Crypto Amount</td>
-                <td>{cryptoAmount}</td>
-              </tr>
-              <tr>
-                <td>Is Buy or Sell</td>
-                <td>{isBuyOrSell}</td>
-              </tr>
-              <tr>
-                <td>Status</td>
-                <td>{status}</td>
-              </tr>
-              <tr>
-                <td>Wallet Address</td>
-                <td>{walletAddress}</td>
-              </tr>
-              <tr>
-                <td>Total Fee in Fiat</td>
-                <td>{totalFeeInFiat}</td>
-              </tr>
-              <tr>
-                <td>Is NFT Order</td>
-                <td>{isNFTOrder}</td>
-              </tr>
-              <tr>
-                <td>Network</td>
-                <td>{network}</td>
-              </tr>
-            </tbody>
-          </table>
-          <Button text="Back to home" onClick={() => navigate("/")} />
-        </>
-      ) : (
-        <>
-          <p>Query parameters missing</p>
-          <Button text="Back to home" onClick={() => navigate("/")} />
-        </>
-      )}
+      <div className="table-container">
+        {areParamsPresent ? (
+          <>
+            <table className="styled-table">
+              <tbody>
+                <tr>
+                  <td>Order ID</td>
+                  <td>{orderId}</td>
+                </tr>
+                <tr>
+                  <td>Fiat Currency</td>
+                  <td>{fiatCurrency}</td>
+                </tr>
+                <tr>
+                  <td>Crypto Currency</td>
+                  <td>{cryptoCurrency}</td>
+                </tr>
+                <tr>
+                  <td>Fiat Amount</td>
+                  <td>{fiatAmount}</td>
+                </tr>
+                <tr>
+                  <td>Crypto Amount</td>
+                  <td>{cryptoAmount}</td>
+                </tr>
+                <tr>
+                  <td>Is Buy or Sell</td>
+                  <td>{isBuyOrSell}</td>
+                </tr>
+                <tr>
+                  <td>Status</td>
+                  <td>{status}</td>
+                </tr>
+                <tr>
+                  <td>Wallet Address</td>
+                  <td>{walletAddress}</td>
+                </tr>
+                <tr>
+                  <td>Total Fee in Fiat</td>
+                  <td>{totalFeeInFiat}</td>
+                </tr>
+                <tr>
+                  <td>Is NFT Order</td>
+                  <td>{isNFTOrder}</td>
+                </tr>
+                <tr>
+                  <td>Network</td>
+                  <td>{network}</td>
+                </tr>
+              </tbody>
+            </table>
+            <Button text="Back to home" onClick={() => navigate("/")} />
+          </>
+        ) : (
+          <>
+            <p className="text">
+              Query parameters missing: {missingParams.join(", ")}
+            </p>
+            <Button text="Back to home" onClick={() => navigate("/")} />
+          </>
+        )}
+      </div>
     </div>
   );
 }
